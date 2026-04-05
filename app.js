62034f2447ea499a84918f3ef0cd1e30de6dd32e
skid-wave <opkingf2@gmail.com>
2026-04-05 12:58:33 +0530

    todo app

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('To-Do App Running via Jenkins CI/CD 🚀');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
